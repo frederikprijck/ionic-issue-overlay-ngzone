5f01d3a7c0160363519e9e178d85182128b1bc10
Frederik Prijck <frederik.prijck@gmail.com>
2018-10-11 21:38:48 +0200

    add code to reproduce issue

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, NgZone, OnInit} from '@angular/core';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  constructor(
+      private alertCtrl: AlertController
+  ) { }
 
+  ngOnInit() {
+      console.log(`Is in Angular Zone (outer): ${NgZone.isInAngularZone()}`);
+      this.alertCtrl
+          .create({
+              buttons: [
+                  {
+                      text: 'OK',
+                      handler: () => {
+                          console.log(`Is in Angular Zone (inner): ${NgZone.isInAngularZone()}`);
+                      }
+                  }
+              ]
+          })
+          .then(alert => {
+              alert.present();
+          });
+  }
 }
